026d53e365d908607c4b9831694a19c11ce2afe4
HoJun Sung <sunghojn@naver.com>
2024-02-27 23:40:42 +0900

    clear start manager script

--- HoJun_Project/Assets/Scripts/StartManager.cs
+++ HoJun_Project/Assets/Scripts/StartManager.cs
@@ -4,26 +4,12 @@ using UnityEngine.UI;
 
 public class StartManager : MonoBehaviour
 {
-    [SerializeField] private Image LockImg;
-    [SerializeField] private Button OnOffBtn;
+    [SerializeField] private Button startBtn;
+    [SerializeField] private Image fadeImg;
 
     private void Awake()
     {
-        OnOffBtn.onClick.AddListener(OnClickStartBtn);
-    }
-
-    private void OnClickOnOffButton()
-    {
-        if (LockImg.isActiveAndEnabled == true)
-        {
-            // LockImg가 켜져있다면
-            LockImg.gameObject.SetActive(false);
-        }
-        else
-        {
-            // LockImg가 꺼져있다면
-            LockImg.gameObject.SetActive(true);
-        }
+        startBtn.onClick.AddListener(OnClickStartBtn);
     }
 
 
