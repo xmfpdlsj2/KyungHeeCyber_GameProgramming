ce3bf34a4ee963637c7bde6a883293a0d2f9dc31
HoJun Sung <sunghojn@naver.com>
2024-03-13 23:25:09 +0900

    add player movement

---
+++ HoJun_Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float _MoveSpeed = 10F;
+    [SerializeField] private float _RotateSpeed = 75F;
+
+    private float _VInput;
+    private float _HInput;
+
+    private void Update()
+    {
+        _VInput = Input.GetAxis("Vertical") * _MoveSpeed;
+        _HInput = Input.GetAxis("Horizontal") * _RotateSpeed;
+
+        this.transform.Translate(Vector3.forward * _VInput * Time.deltaTime);
+        this.transform.Rotate(Vector3.up * _HInput * Time.deltaTime);
+    }
+}
