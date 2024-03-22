3a3ef13ebccf0b31b0032b8a0f586ec67f521715
HoJun Sung <sunghojn@naver.com>
2024-03-22 21:32:27 +0900

    add jump functions

--- HoJun_Project/Assets/Scripts/PlayerMovement.cs
+++ HoJun_Project/Assets/Scripts/PlayerMovement.cs
@@ -9,25 +9,55 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField] private Rigidbody _Rb;
 
+    [SerializeField] private LayerMask _LayerMask;
+    [SerializeField] private CapsuleCollider _CapCol;
+
+    // 이동
     private float _VInput;
     private float _HInput;
 
+    // 점프
+    private float _JumpVelocity = 5F;
+    private bool _IsJump = false;
+    private float _DistanceToGround = 0.1f;
+
     private void Update()
     {
+        // 이동
         _VInput = Input.GetAxis("Vertical") * _MoveSpeed;
         _HInput = Input.GetAxis("Horizontal") * _RotateSpeed;
 
         this.transform.Translate(Vector3.forward * _VInput * Time.deltaTime);
         this.transform.Rotate(Vector3.up * _HInput * Time.deltaTime);
+
+        // 점프
+        _IsJump |= Input.GetKeyDown(KeyCode.Space);
     }
 
     private void FixedUpdate()
     {
-        
+        if (IsGrounded() && _IsJump) 
+        {
+            Jump();
+        }
+
+        _IsJump = false;
     }
 
     private void Jump()
     {
+        _Rb.AddForce(Vector3.up * _JumpVelocity, ForceMode.Impulse);
+    }
 
+    private bool IsGrounded()
+    {
+        Vector3 capsuleBottom = new Vector3(_CapCol.bounds.center.x,
+            _CapCol.bounds.min.y, _CapCol.bounds.center.z);
+
+        bool isGround = Physics.CheckCapsule(_CapCol.bounds.center,
+            capsuleBottom, _DistanceToGround, _LayerMask,
+            QueryTriggerInteraction.Ignore);
+
+        return isGround;
     }
 }
